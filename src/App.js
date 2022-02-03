a92842cb7f02d423fad865c646ced35e5df95aa7
moraes <moraeslua66@gmail.com>
2022-02-03 15:38:43 -0300

    Adiciona Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Content from './Content';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Content />
+      </BrowserRouter>
+    );
   }
 }
 
