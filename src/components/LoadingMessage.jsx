d4a1f96c0542b98030520911cd3cde3f41831e79
moraes <moraeslua66@gmail.com>
2022-02-04 19:13:00 -0300

    Adiciona Requisito 2

---
+++ src/components/LoadingMessage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class LoadingMessage extends React.Component {
+  render() {
+    return (<h3>Carregando...</h3>);
+  }
+}
+
+export default LoadingMessage;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import { createUser } from '../services/userAPI';
+import LoadingMessage from '../components/LoadingMessage';
+import FormLogin from '../components/FormLogin';
 
 class Login extends React.Component {
+  state = {
+    loading: false,
+    loginName: '',
+    loginButtonIsDisabled: true,
+    shouldRedirect: false,
+  };
+
+  handleOnChange = ({ target }) => {
+    const { id, value } = target;
+    this.setState({
+      [id]: value,
+    }, () => this.handleLoginButtonState());
+  }
+
+  handleLoginButtonState = () => {
+    const MAX_VALUE = 3;
+    const { loginName } = this.state;
+    this.setState({
+      loginButtonIsDisabled: loginName.length < MAX_VALUE,
+    });
+  }
+
+  handleSubmitButton = async () => {
+    const { loginName } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name: loginName });
+    this.setState({ loading: false, shouldRedirect: true });
+  }
+
   render() {
+    const { loginName, loginButtonIsDisabled, loading, shouldRedirect } = this.state;
     return (
-      <div data-testid="page-login">
-        <p>Teste</p>
+      <div>
+        {loading ? <LoadingMessage /> : <FormLogin
+          loginName={ loginName }
+          loginButtonIsDisabled={ loginButtonIsDisabled }
+          onChange={ this.handleOnChange }
+          onClick={ this.handleSubmitButton }
+        />}
+        {shouldRedirect && <Redirect to="/search" />}
       </div>
     );
   }
