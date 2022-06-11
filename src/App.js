53a5b74e38c0afc07f196afd920a918355fc2d23
moraes <moraeslua66@gmail.com>
2022-06-11 15:47:42 -0300

    refactor: move routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Content from './Content';
+import { ThemeProvider } from 'styled-components';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import NotFound from './pages/NotFound';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import GlobalStyle from './assets/styles/global';
+import defaultTheme from './assets/styles/themes/default';
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Content />
-      </BrowserRouter>
+      <ThemeProvider theme={ defaultTheme }>
+        <BrowserRouter>
+          <GlobalStyle />
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="/search" component={ Search } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route path="/profile" component={ Profile } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
     );
   }
 }
