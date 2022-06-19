864012673431c33fc2fbce384b78d8d5bd37d848
moraes <moraeslua66@gmail.com>
2022-06-19 15:16:15 -0300

    refactor: transfer config from app.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import MusicPlayerProvider from './context/MusicPlayerProvider';
+import { ThemeProvider } from 'styled-components';
+import defaultTheme from './assets/styles/themes/default';
+import GlobalStyle from './assets/styles/global';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MusicPlayerProvider>
+      <GlobalStyle />
+      <ThemeProvider theme={ defaultTheme }>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </MusicPlayerProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
