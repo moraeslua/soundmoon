ed868a04d40718f84a3fd713c412688c29ae695c
moraes <moraeslua66@gmail.com>
2022-06-19 15:13:23 -0300

    feat: spinner on loading component

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import spinner from '../../assets/images/spinner.svg';
+import * as C from './styles';
 
 class LoadingMessage extends React.Component {
   render() {
-    return (<h3>Carregando...</h3>);
+    return (
+      <C.Container>
+        <C.SpinnerContainer>
+          <C.Spinner src={ spinner } alt="carregando" />
+        </C.SpinnerContainer>
+      </C.Container>
+    );
   }
 }
 
---
+++ src/components/Loading/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Spinner = styled.img`
+  width: 60px;
+  height: 60px;
+`;
+
+export const Container = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
+export const SpinnerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: calc(100vh - 170px);
+`;
